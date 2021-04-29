757e089f9abf8bd7949082a25972290a98bbc738
huszarpp <huszarpp@gmail.com>
2021-04-29 11:32:12 +0200

    Implement Army

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
@@ -1,3 +1,35 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Army {}
+import java.util.ArrayList;
+import java.util.List;
+
+class Army {
+
+    List<MilitaryUnit> militaryUnitsOfArmy = new ArrayList<>();
+
+    void addUnit(MilitaryUnit militaryUnit) {
+        militaryUnitsOfArmy.add(militaryUnit);
+    }
+
+    void damageAll(int damage) {
+        for (int i = 0; i < militaryUnitsOfArmy.size(); i++) {
+            militaryUnitsOfArmy.get(i).sufferDamage(damage);
+            if (militaryUnitsOfArmy.get(i).isDeadOrIncapableOfFighting()) {
+                militaryUnitsOfArmy.remove(i);
+                i--;
+            }
+        }
+    }
+
+    int getArmyDamage() {
+        int sumOfDamage = 0;
+        for (MilitaryUnit militaryUnit : militaryUnitsOfArmy) {
+            sumOfDamage += militaryUnit.doDamage();
+        }
+        return sumOfDamage;
+    }
+
+    int getArmySize() {
+        return militaryUnitsOfArmy.size();
+    }
+}
